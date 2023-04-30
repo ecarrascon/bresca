d5803e10139628901bd143802552ca0ad56a769c
Eduardo Carrascon <carrascon.eduardo@gmail.com>
2023-04-30 21:26:29 +0200

    Logout button implemented

--- app/src/main/java/dev/carrascon/bresca/PrincipalActivity.java
+++ app/src/main/java/dev/carrascon/bresca/PrincipalActivity.java
@@ -6,18 +6,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+
 public class PrincipalActivity extends AppCompatActivity {
 
     private Button btnGoToUpload;
     private Button btnGoToWatch;
+    private Button btnLogout;
+    private FirebaseAuth auth;
+    private GoogleSignInClient managerGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
+        auth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        managerGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         btnGoToUpload = findViewById(R.id.btnGoToUpload);
         btnGoToWatch = findViewById(R.id.btnGoToWatch);
+        btnLogout = findViewById(R.id.btnLogout);
 
         btnGoToWatch.setOnClickListener(v -> {
             Intent watchIntent = new Intent(PrincipalActivity.this, VideoFeedActivity.class);
@@ -27,6 +45,15 @@ public class PrincipalActivity extends AppCompatActivity {
         btnGoToUpload.setOnClickListener(v -> {
             Intent uploadIntent = new Intent(PrincipalActivity.this, UploadVideoActivity.class);
             startActivity(uploadIntent);
+        });
+
+        btnLogout.setOnClickListener(v -> {
+            auth.signOut();
+            managerGoogleSignInClient.signOut();
+
+            Intent logoutIntent = new Intent(PrincipalActivity.this, MainActivity.class);
+            startActivity(logoutIntent);
+            finish();
         });
     }
 }
